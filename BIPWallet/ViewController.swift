a8be41ed745418bbadd0892aa2031a952e36f747
Alexey Sidorov <ody344@gmail.com>
2020-02-07 18:41:20 +0530

    Initial Commit

---
+++ BIPWallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BIPWallet
+//
+//  Created by Alexey Sidorov on 07.02.2020.
+//  Copyright © 2020 Alexey Sidorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
